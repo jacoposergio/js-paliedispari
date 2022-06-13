b1a6638f7c2836626cb03bf18638c3a0c866e491
jacoposergio <djago32@gmail.com>
2022-06-13 15:36:07 +0200

    split array

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -1,1 +1,39 @@
-alert('tappost')
+// TODO  Chiedere all’utente di inserire una parola
+// TODO  Creare una funzione per capire se la parola inserita è palindroma
+
+// creo un array vuoto
+// const userWord =  parseInt(prompt('Dimmi una parola'));
+// const userWordPalindrome = isPalindrome(userWord);
+// console.log("",userWord);
+
+// if( userWordPalindrome === palindromeTrue){
+//     alert('è una parola palindroma')
+// } else{
+//     alert('non è una parola palindroma')
+// }
+
+// funzione che verifica se la parola inserita è palindroma 
+
+
+// function isPalindrome(userWord){
+
+// }
+
+
+// creo un array vuoto 
+const userWordArray = [];
+// chiedo una parola all'utente
+const userWord = prompt('dimmi una parola');
+console.log("parola utente",userWord);        //console log della parola
+
+// pusho la parola dell'utente nell 'array 
+userWordArray.push(userWord.split(""));
+console.log("array splittato",userWordArray);   // array
+
+// const splitUserWordArray = userWordArray.split;
+// console.log("",splitUserWordArray);
+
+
+
+
+
